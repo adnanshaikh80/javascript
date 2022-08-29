15dc803508b15a44a28c00f4010586391a0d0287
adnan <adnanshaik80@gmail.com>
2022-08-29 22:24:27 +0530

    logic

--- script.js
+++ script.js
@@ -27,3 +27,15 @@ var image2 = document.querySelectorAll("img")[1];
 console.log(image2);
 
 image2.setAttribute("src",randomImageSource2);
+
+//WINNER
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🎉Player 1 Wins!";
+  }
+  
+  else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = " 🎉Player 2 Wins!"
+  }
+  else{
+      document.querySelector("h1").innerHTML = "🎉Draw!"
+  }
